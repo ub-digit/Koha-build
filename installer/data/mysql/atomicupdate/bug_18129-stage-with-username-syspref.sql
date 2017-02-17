71cf8660fbb1f1819bbc207e13b511a74c0e1e38
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2017-02-17 15:50:45 +0100

    Add borrowernumber to staged imports, and set it to the currently logged in user. SysPref to turn on filtering of staged import list so that only batches staged by logged in user is visible. SysPref to remove Cleaned/Imported batches from staged import list. Link near list to ignore filter, in case one needs to see all batches.
    https://bugs.koha-community.org/show_bug.cgi?id=18129

---
+++ installer/data/mysql/atomicupdate/bug_18129-stage-with-username-syspref.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES ('StageFilterByUser', '0', '', 'Filter staged batches by user', 'YesNo');
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES ('StageHideCleanedImported', '0', '', 'Hide staged batches when cleaned or imported', 'YesNo');
