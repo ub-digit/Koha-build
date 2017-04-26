3e73a5258244f1ec0ae3664918bbde3909fe4a45
Johan Larsson <johan.larsson@ub.gu.se>
2017-04-26 10:43:40 +0200

    added syspref for enabling the simplified messaging option in opac
    added code to simulate the queryparams if simplifeid messaging is enabled
    
    added the simplifed form for messaging. Hiding the original form also
    
    Intranet is now using the simplified version of the messaging preferences
    
    added the simplifed form for messaging. Hiding the original form also
    
    removed blank lines
    
    added new syspref and using it to set the correct notice methods for listed events
    
    added ticking the methods based on syspref
    
    added syspref for enabling the simplified messaging option in opac
    
    added code to simulate the queryparams if simplifeid messaging is enabled
    
    added the simplifed form for messaging. Hiding the original form also
    
    added syspref for enabling the simplified messaging option in opac
    
    added the simplifed form for messaging. Hiding the original form also
    
    Intranet is now using the simplified version of the messaging preferences
    
    removed blank lines
    
    added new syspref and using it to set the correct notice methods for listed events
    
    added ticking the methods based on syspref
    
    conflicts removed
    
    moved code for handeling simplified messaging to hande both insert and update
    
    removed conflict
    
    added latest code from master
    
    added code from master
    
    Text changes
    
    added support for days-in-advanced
    
    added info about email and smsnumber on messaging page
    
    moved code to correct place

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -33,6 +33,7 @@ use C4::Members;
 use C4::Koha;
 use C4::Log;
 use C4::Letters;
+use C4::Members::Messaging;
 use C4::Form::MessagingPreferences;
 use Koha::AuthUtils;
 use Koha::AuthorisedValues;
@@ -54,7 +55,9 @@ use vars qw($debug);
 BEGIN {
 	$debug = $ENV{DEBUG} || 0;
 }
-	
+
+my $messaging_options = C4::Members::Messaging::GetMessagingOptions();
+
 my $input = new CGI;
 ($debug) or $debug = $input->param('debug') || 0;
 my %data;
@@ -427,6 +430,55 @@ if ( ($op eq 'modify' || $op eq 'insert' || $op eq 'save'|| $op eq 'duplicate') 
     unless ($newdata{'dateexpiry'}){
         my $patron_category = Koha::Patron::Categories->find( $newdata{categorycode} );
         $newdata{'dateexpiry'} = $patron_category->get_expiry_date( $newdata{dateenrolled} ) if $patron_category;
+    }
+}
+
+# do this for both save and insert  if EnhancedMessagingPreferences and simple-messaging is used
+if ( $op eq 'insert' || $op eq 'save') {
+    # if simplified form is to be used we add the params here
+    if (C4::Context->preference('EnhancedMessagingPreferences') and $input->param('setting_messaging_prefs')) {
+      if ( defined $input->param('simple-messaging') && $input->param('simple-messaging') eq 'yes') {
+          if (defined $input->param ('opacmessaging-simple-radios')) {
+              my %whichActionsToTickUsingSimpleOpacMessaging = map { $_ => 1 } (split /\|/, C4::Context->preference('whichActionsToTickUsingSimpleOpacMessaging')); # split the string to array and then convert to hash to use keys for easy checking
+              if ($input->param('opacmessaging-simple-radios') eq 'sms') {
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleOpacMessaging{$messaging_option->{'message_name'}}) {
+                        $input->param($messaging_option->{'message_attribute_id'}, "sms");
+
+                    }
+                  }
+
+              }
+              elsif ($input->param('opacmessaging-simple-radios') eq 'email') {
+                  # set all types to email
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleOpacMessaging{$messaging_option->{'message_name'}}) {
+                         $input->param($messaging_option->{'message_attribute_id'}, "email");
+                    }
+                  }
+              }
+              elsif (($input->param('opacmessaging-simple-radios') eq 'SmsAndEmail')) {
+                   # set all types to email and sms
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleOpacMessaging{$messaging_option->{'message_name'}}) {
+                      $input->param($messaging_option->{'message_attribute_id'}, "sms", "email");
+                    }
+                  }
+              }
+              elsif (($input->param('opacmessaging-simple-radios') eq 'paper')) {
+                   # set all types to do not notify
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleOpacMessaging{$messaging_option->{'message_name'}}) {
+                      $input->delete($messaging_option->{'message_attribute_id'});
+                    }
+                  }
+              }
+          }
+      }
     }
 }
 
