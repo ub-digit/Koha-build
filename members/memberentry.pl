d4b0e74ff5e2d261787f299781e169aace25dd70
Johan Larsson <johan.larsson@ub.gu.se>
2017-04-26 10:43:40 +0200

    Add a simplified messaging functionallity option
    enableSimpleMessaging was not indented correctly

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -31,6 +31,7 @@ use C4::Output qw( output_and_exit output_and_exit_if_error output_html_with_htt
 use C4::Members qw( checkcardnumber get_cardnumber_length );
 use C4::Koha qw( GetAuthorisedValues );
 use C4::Letters qw( SendAlerts );
+use C4::Members::Messaging;
 use C4::Form::MessagingPreferences;
 use Koha::AuthUtils;
 use Koha::AuthorisedValues;
@@ -47,6 +48,8 @@ use Koha::Patron::HouseboundRoles;
 use Koha::Plugins;
 use Koha::Token;
 use Koha::SMS::Providers;
+
+my $messaging_options = C4::Members::Messaging::GetMessagingOptions();
 
 my $input = CGI->new;
 my %data;
@@ -428,6 +431,57 @@ if ( ($op eq 'modify' || $op eq 'insert' || $op eq 'save'|| $op eq 'duplicate') 
     unless ($newdata{'dateexpiry'}){
         my $patron_category = Koha::Patron::Categories->find( $newdata{categorycode} );
         $newdata{'dateexpiry'} = $patron_category->get_expiry_date( $newdata{dateenrolled} ) if $patron_category;
+    }
+}
+
+# do this for both save and insert  if EnhancedMessagingPreferences and simple-messaging is used
+if ( $op eq 'insert' || $op eq 'save') {
+    # if simplified form is to be used we add the params here
+    if (C4::Context->preference('EnhancedMessagingPreferences') and $input->param('setting_messaging_prefs')) {
+      if ( defined $input->param('simple-messaging') && $input->param('simple-messaging') eq 'yes') {
+          if (defined $input->param ('messaging-simple-radios')) {
+              my %whichActionsToTickUsingSimpleMessaging = map { $_ => 1 } (split /\|/, C4::Context->preference('whichActionsToTickUsingSimpleMessaging')); # split the string to array and then convert to hash to use keys for easy checking
+              if ($input->param('messaging-simple-radios') eq 'sms') {
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleMessaging{$messaging_option->{'message_name'}}) {
+                        $input->param($messaging_option->{'message_attribute_id'}, "sms");
+
+                    }
+                  }
+
+              }
+              elsif ($input->param('messaging-simple-radios') eq 'email') {
+                  # set all types to email
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleMessaging{$messaging_option->{'message_name'}}) {
+                         $input->param($messaging_option->{'message_attribute_id'}, "email");
+                    }
+                  }
+              }
+              elsif (($input->param('messaging-simple-radios') eq 'SmsAndEmail')) {
+                   # set all types to email and sms
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleMessaging{$messaging_option->{'message_name'}}) {
+                      $input->param($messaging_option->{'message_attribute_id'}, "sms", "email");
+                    }
+                  }
+              }
+              elsif (($input->param('messaging-simple-radios') eq 'paper')) {
+                   # set all types to do not notify
+                  foreach my $messaging_option (@{$messaging_options})
+                  {
+                    if ($whichActionsToTickUsingSimpleMessaging{$messaging_option->{'message_name'}}) {
+                      $input->delete($messaging_option->{'message_attribute_id'});
+                    }
+                  }
+              }
+          }
+      }
+      delete $newdata{"messaging-simple-radios"};
+      delete $newdata{"simple-messaging"};
     }
 }
 
