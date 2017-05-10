374d08ad3512067696bfed892eb8ad1f7c1c28da
David Gustafsson <david.gustafsson@ub.gu.se>
2017-05-10 18:50:24 +0200

    Fix non fixed length primary key

--- koha_deploy/instance_migrations_schema.sql
+++ koha_deploy/instance_migrations_schema.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS koha_deploy_instance_migrations (
-  revision TEXT PRIMARY KEY,
+  revision VARCHAR(255) PRIMARY KEY,
   migration_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
