0744f0f9cab0b6e97a0977a6b40d2a2e0e00f1be
David Gustafsson <david.gustafsson@ub.gu.se>
2017-05-10 18:54:12 +0200

    Fix older mysql versions does not support default now for datetime

--- koha_deploy/instance_migrations_schema.sql
+++ koha_deploy/instance_migrations_schema.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS koha_deploy_instance_migrations (
   revision VARCHAR(255) PRIMARY KEY,
-  migration_date DATETIME DEFAULT CURRENT_TIMESTAMP
+  migration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
