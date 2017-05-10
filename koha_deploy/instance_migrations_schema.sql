752b6235011b08a9bbb6ccae68dce7ac31614337
David Gustafsson <david.gustafsson@ub.gu.se>
2017-05-10 18:38:18 +0200

    Fix so instance_migrations_schema.sql can be run multiple times without issues

--- koha_deploy/instance_migrations_schema.sql
+++ koha_deploy/instance_migrations_schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE koha_deploy_instance_migrations (
+CREATE TABLE IF NOT EXISTS koha_deploy_instance_migrations (
   revision TEXT PRIMARY KEY,
   migration_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
