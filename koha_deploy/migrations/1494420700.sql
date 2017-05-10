2a0be7026069e43d9f0cdffbf87059fe1825ac4b
David Gustafsson <david.gustafsson@ub.gu.se>
2017-05-10 19:10:38 +0200

    Add some testmigrations

---
+++ koha_deploy/migrations/1494420700.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS testing2 (
+  id MEDIOMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  value TEXT
+);
+INSERT INTO testing2(value) VALUES('test3');
+INSERT INTO testing2(value) VALUES('test4');
