8e4581182a5a0269b7635c1e840e2b83eb0839f7
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2017-09-12 12:47:00 +0200

    Add plugin-hook before building search query for elasticsearch. Create a run_matching in Koha::Plugins::Handler to run all plugins with a method matching build_query_before which is the hook name
    Bug in run_matching when plugins are not available
    
    Add hooks for add_reserve_before and add_reserve_after
    
    Add hook for update_index_before
    
    Added missing use statement.
    
    - In order to use the Handler for plugins in the hook
      we need to include the code package.

--- C4/Reserves.pm
+++ C4/Reserves.pm
@@ -46,6 +46,8 @@ use Koha::Libraries;
 use Koha::Old::Hold;
 use Koha::Patrons;
 use Koha::Plugins;
+use Koha::CirculationRules;
+use Koha::Plugins::Handler;
 
 use Carp;
 use Data::Dumper;
@@ -187,6 +189,43 @@ sub AddReserve {
     my $itemtype       = $params->{itemtype};
     my $non_priority   = $params->{non_priority};
 
+    my $plugin_result = Koha::Plugins::Handler->run_hook(
+        {
+            method => 'add_reserve_before',
+            params => {
+                branch => $branch,
+                borrowernumber => $borrowernumber,
+                biblionumber => $biblionumber,
+                priority => $priority,
+                resdate => $resdate,
+                expdate => $expdate,
+                notes => $notes,
+                title => $title,
+                checkitem => $checkitem,
+                found => $found,
+                itemtype => $itemtype
+            }
+        }
+        );
+
+    (
+     $branch,   $borrowernumber, $biblionumber,
+     $priority, $resdate,        $expdate,      $notes,
+     $title,    $checkitem,      $found,        $itemtype) = (
+        $plugin_result->{'branch'},
+        $plugin_result->{'borrowernumber'},
+        $plugin_result->{'biblionumber'},
+        $plugin_result->{'priority'},
+        $plugin_result->{'resdate'},
+        $plugin_result->{'expdate'},
+        $plugin_result->{'notes'},
+        $plugin_result->{'title'},
+        $plugin_result->{'checkitem'},
+        $plugin_result->{'found'},
+        $plugin_result->{'itemtype'}
+    );
+
+
     $resdate = output_pref( { str => dt_from_string( $resdate ), dateonly => 1, dateformat => 'iso' })
         or output_pref({ dt => dt_from_string, dateonly => 1, dateformat => 'iso' });
 
@@ -256,6 +295,17 @@ sub AddReserve {
 
     logaction( 'HOLDS', 'CREATE', $hold->id, Dumper($hold->unblessed) )
         if C4::Context->preference('HoldsLog');
+
+    $plugin_result = Koha::Plugins::Handler->run_hook(
+        {
+            method => 'add_reserve_after',
+            params => {
+                hold => $hold
+            }
+        }
+        );
+
+    $hold = $plugin_result->{'hold'};
 
     my $reserve_id = $hold->id();
 
