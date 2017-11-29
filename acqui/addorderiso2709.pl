9b16da1e14cd472bebbaa729cb28afa715a759ec
Johan Larsson <johan.larsson@ub.gu.se>
2017-11-29 13:38:54 +0100

    Recalculate price based on currency

--- acqui/addorderiso2709.pl
+++ acqui/addorderiso2709.pl
@@ -53,6 +53,7 @@ use Koha::Acquisition::Baskets;
 use Koha::Acquisition::Currencies;
 use Koha::Acquisition::Orders;
 use Koha::Acquisition::Booksellers;
+use Koha::Acquisition::Currencies;
 use Koha::Patrons;
 
 my $input = CGI->new;
@@ -274,6 +275,11 @@ if ($op eq ""){
                         # in this case, the price will be x100 when unformatted ! Replace the . by a , to get a proper price calculation
                         $price =~ s/\./,/ if C4::Context->preference("CurrencyFormat") eq "FR";
                         $price = Koha::Number::Price->new($price)->unformat;
+                        # RECALCULATE PRICE BASED ON CURRENCY
+                        my $currency_code = $bookseller->listprice;
+                        my $currency = Koha::Acquisition::Currencies->find($currency_code);
+                        my $currency_rate = $currency->rate;
+                        $price = $price * $currency_rate;
                         $orderinfo{tax_rate} = $bookseller->tax_rate;
                         my $c = $c_discount ? $c_discount : $bookseller->discount / 100;
                         $orderinfo{discount} = $c;
@@ -335,6 +341,11 @@ if ($op eq ""){
                 # in this case, the price will be x100 when unformatted ! Replace the . by a , to get a proper price calculation
                 $c_price =~ s/\./,/ if C4::Context->preference("CurrencyFormat") eq "FR";
                 $c_price = Koha::Number::Price->new($c_price)->unformat;
+                # RECALCULATE PRICE BASED ON CURRENCY
+                my $currency_code = $bookseller->listprice;
+                my $currency = Koha::Acquisition::Currencies->find($currency_code);
+                my $currency_rate = $currency->rate;
+                $c_price = $c_price * $currency_rate;
                 $orderinfo{tax_rate} = $bookseller->tax_rate;
                 my $c = $c_discount ? $c_discount : $bookseller->discount / 100;
                 $orderinfo{discount} = $c;
