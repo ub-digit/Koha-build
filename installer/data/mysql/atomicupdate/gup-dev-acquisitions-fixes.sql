38c9fbe277517708036c72bf398fe20c4ae3a5c1
Johan Larsson <johan.larsson@ub.gu.se>
2017-11-29 13:38:54 +0100

    calculate prices before save and only require values selected for selected rows
    added price code to work for item also
    
    added syspref for toggeling uncertain prices when importing from staged file
    
    using syspref to toggle uncertainprice on/of
    
    removed content-scripts

---
+++ installer/data/mysql/atomicupdate/gup-dev-acquisitions-fixes.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('AcqUncertainPriceWhenImporting','1',NULL,'if yes it will set the uncertainprice to 1 else 0','YesNo');
