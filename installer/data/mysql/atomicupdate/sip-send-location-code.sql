7721e4680f7ebb726bce619bf641f346968956c9
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-01-15 14:20:06 +0100

    Add option to send permanent_location as AQ in SIP response

---
+++ installer/data/mysql/atomicupdate/sip-send-location-code.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('UseLocationAsAQInSIP', '0', '', 'Use permanent_location instead of homebranch for AQ in SIP response', 'YesNo');
