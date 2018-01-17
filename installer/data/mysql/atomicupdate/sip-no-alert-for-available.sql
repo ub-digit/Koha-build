77e8e5d2381e97ea1aced140595e9b7947b3b95f
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-01-17 10:55:45 +0100

    Syspref for not setting alert flag when book is available (not on loan)

---
+++ installer/data/mysql/atomicupdate/sip-no-alert-for-available.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('SipNoAlertForAvailable', '0', '', 'Do not set alert flag when NotIssued is returned', 'YesNo');
