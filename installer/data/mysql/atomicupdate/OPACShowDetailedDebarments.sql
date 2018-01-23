2dd0ff3ae11244d003736f62da86d669cc5bcc95
Torgny Rasmark <Torgny.Rasmark@ub.gu.se>
2018-01-23 14:00:08 +0100

    KOHA-925 ready
    cleaning up code
    
    atomicupdate insert line for syspref OPACShowDetailedDebarments
    
    KOHA-925 bugfix, OVERDUE_PROCESS
    
    Gub dev koha 925 (#74)
    
    * fixed messages and links for debarments
    
    * added class
    
    Allow renewals despite overdues restriction
    
    Allow renewals despite overdues restriction

---
+++ installer/data/mysql/atomicupdate/OPACShowDetailedDebarments.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('OPACShowDetailedDebarments', '0', '', 'Show detailed debarments messages', 'YesNo');
