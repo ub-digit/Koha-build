81c29d67206cb3caf99515e15bea8601a04bec07
Torgny Rasmark <Torgny.Rasmark@ub.gu.se>
2018-01-23 14:00:08 +0100

    KOHA-925 ready
    cleaning up code
    
    atomicupdate insert line for syspref OPACShowDetailedDebarments
    
    KOHA-925 bugfix, OVERDUE_PROCESS
    
    Gub dev koha 925 (#74)
    
    * fixed messages and links for debarments
    
    * added class
    
    Allow renewals despite overdues restriction
    
    Allow renewals despite overdues restriction

--- opac/opac-user.pl
+++ opac/opac-user.pl
@@ -106,6 +106,25 @@ my ($warning_year, $warning_month, $warning_day) = split /-/, $borr->{'dateexpir
 
 my $debar = Koha::Patrons->find( $borrowernumber )->is_debarred;
 my $userdebarred;
+my $allowed_despite_restriction = 1;
+if (C4::Context->preference( 'OPACShowDetailedDebarments')) {
+    our $restrictionCodeList = [];
+    if ( $borr->{'gonenoaddress'}){push $restrictionCodeList, "ADDRESS_MISSING"};
+    if ( $borr->{'lost'})         {push $restrictionCodeList, "CARD_LOST"};
+    my $debarments = Koha::Patron::Debarments::GetDebarments({ borrowernumber => $borrowernumber});
+    my ($hashref, $key, $cmt);
+    my $borrstring = my $commentstring = '';
+    foreach $hashref (@$debarments){
+        $commentstring = %$hashref{'comment'};
+        (($key, $cmt) = $commentstring =~m/^(OVERDUES_PROCESS)\s*(.*)$/) || (($key, $cmt) = $commentstring =~ m/^([^, ]+)[, ]+[^:]*(?:: (.*))?$/);
+        if($key ne "OVERDUES_PROCESS") {
+            $allowed_despite_restriction = 0;
+        }
+        $borr->{"${key}comment"}  = $cmt;
+        push $restrictionCodeList, $key;
+    }
+    $borr->{'restrictionCodeList'}    = $restrictionCodeList;
+};
 
 if ($debar) {
     $userdebarred = 1;
@@ -122,7 +141,7 @@ if ($debar) {
     $template->param( 'discharge_available' => $available && Koha::Patron::Discharge::is_discharged({borrowernumber => $borrowernumber}) );
 }
 
-if ( $userdebarred || $borr->{'gonenoaddress'} || $borr->{'lost'} ) {
+if ( ($userdebarred  && !$allowed_despite_restriction) || $borr->{'gonenoaddress'} || $borr->{'lost'} ) {
     $borr->{'flagged'} = 1;
     $canrenew = 0;
 }
@@ -139,6 +158,8 @@ if (   C4::Context->preference('OpacRenewalAllowed')
     && defined($no_renewal_amt)
     && $amountoutstandingfornewal > $no_renewal_amt )
 {
+if (C4::Context->preference( 'OPACShowDetailedDebarments')) {push our $restrictionCodeList, "FINES_EXCEEDED";}
+
     $borr->{'flagged'} = 1;
     $canrenew = 0;
     $template->param(
@@ -312,6 +333,7 @@ $template->param( ISSUES       => \@issuedat );
 $template->param(has_at_least_one_issue_auto_renewal => $has_at_least_one_issue_auto_renewal);
 $template->param( issues_count => $count );
 $template->param( canrenew     => $canrenew );
+$template->param( allowed_despite_restriction => $allowed_despite_restriction);
 $template->param( OVERDUES       => \@overdues );
 $template->param( overdues_count => $overdues_count );
 
