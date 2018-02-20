d4d96e523c7128db73f26789926dd05ebb8fa858
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-02-20 13:59:49 +0100

    Add syspref with list of NOT_LOAN statuses that should prevent triggering of reserves on checkin

---
+++ installer/data/mysql/atomicupdate/prevent-notloan-from-hold-resolve.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('PreventNotLoanFromHoldResolve', '', '', 'List of NotLoan statuses that should not trigger holds on return. Example -3|-2|-1', 'free');
