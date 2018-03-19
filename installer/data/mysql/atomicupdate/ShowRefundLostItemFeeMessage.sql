5835c5be6f89b53e32eec9e727538fe398040340
Lars Berglén <lars.berglen@ub.gu.se>
2018-03-19 11:14:14 +0100

    Syspref for hiding lost item refund fee message
    hide refund message

---
+++ installer/data/mysql/atomicupdate/ShowRefundLostItemFeeMessage.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('ShowRefundLostItemFeeMessage', '1', '', 'Show lost item fee refund message.', 'YesNo');
