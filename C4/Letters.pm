cc5a5aff66169db6c6f9932e948368676bb7790e
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-03-22 09:41:31 +0100

    Add delay option for process_message_queue so that messages will not be sent immediately

--- C4/Letters.pm
+++ C4/Letters.pm
@@ -1019,6 +1019,7 @@ sub SendQueuedMessages {
         'borrowernumber' => $params->{'borrowernumber'} // q{},
         'letter_code'    => $params->{'letter_code'} // q{},
         'type'           => $params->{'type'} // q{},
+        'delay_send'     => $params->{delay_send} // 0,
     };
     my $unsent_messages = _get_unsent_messages( $which_unsent_messages );
     MESSAGE: foreach my $message ( @$unsent_messages ) {
@@ -1310,6 +1311,10 @@ sub _get_unsent_messages {
         if ( $params->{message_id} ) {
             $statement .= ' AND message_id = ?';
             push @query_params, $params->{message_id};
+        }
+        if ( $params->{'delay_send'} ) {
+            $statement .= ' AND time_queued < DATE_SUB(now(), INTERVAL ? MINUTE)';
+            push @query_params, $params->{'delay_send'};
         }
         if ( $params->{'limit'} ) {
             $statement .= ' limit ? ';
