0669cc205f4a5c9703b5ce674aeed325dafbd304
Lars Berglén <lars.berglen@ub.gu.se>
2018-03-22 13:50:38 +0100

    fixed default btn focus on checkin, and keep search term in simple search

--- koha-tmpl/intranet-tmpl/js/gub-intranet.js
+++ koha-tmpl/intranet-tmpl/js/gub-intranet.js
@@ -166,17 +166,18 @@ $(document).ready(function() {
 
   // check if return btn is visible
   setTimeout(function() {
-    if ($('#circ_returns.circ.modal-open .btn.approve').is(':visible')) {
+    if ($('#circ_returns.circ.modal-open .btn.approve.gub-set-focus').is(':visible')) {
       $('.btn.approve').focus();
     }
   }, 300);
 
 
   // Keep search term after search
+
   let val = getQueryVariable('q');
   if (val.length > 0) {
     if (!getQueryVariable('idx')) {
-      $('#search-form').val(val);
+      $('#search-form').val(decodeURIComponent(val.replace(/\+/g, ' ')));
     }
   }
 
