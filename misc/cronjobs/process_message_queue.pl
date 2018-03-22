3f8bf0d0aec4b9c769e0f287c005bc1456bce274
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-03-22 09:41:31 +0100

    Add delay option for process_message_queue so that messages will not be sent immediately

--- misc/cronjobs/process_message_queue.pl
+++ misc/cronjobs/process_message_queue.pl
@@ -38,12 +38,14 @@ my $method = 'LOGIN';
 my $help = 0;
 my $verbose = 0;
 my $type = q{};
+my $delay = 0;
 
 GetOptions(
     'u|username:s'      => \$username,
     'p|password:s'      => \$password,
     'l|limit:s'         => \$limit,
     'm|method:s'        => \$method,
+    'd|delay:s'         => \$delay,
     'h|help|?'          => \$help,
     'v|verbose'         => \$verbose,
     't|type:s'          => \$type,
@@ -62,6 +64,7 @@ This script has the following parameters :
     -t --type: If supplied, only processes this type of message ( email, sms )
     -l --limit: The maximum number of messages to process for this run
     -m --method: authentication method required by SMTP server (See perldoc Sendmail.pm for supported authentication types.)
+    -d --delay: Do not send messages newer than this number of minutes (default 0)
     -h --help: this message
     -v --verbose: provides verbose output to STDOUT
 ENDUSAGE
@@ -78,6 +81,7 @@ C4::Letters::SendQueuedMessages(
         method   => $method,
         limit    => $limit,
         type     => $type,
+        delay_send => $delay,
     }
 );
 
