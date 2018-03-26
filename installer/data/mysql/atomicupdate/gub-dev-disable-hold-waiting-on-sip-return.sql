b2eb596287cb16172efb7877db9eb53a08177b0a
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-03-26 12:52:42 +0200

    Option to disable triggering of hold when checkin via SIP at pickup library, to prevent message from being generated too early

---
+++ installer/data/mysql/atomicupdate/gub-dev-disable-hold-waiting-on-sip-return.sql
@@ -0,0 +1,2 @@
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('TriggerWaitingOnSIPReturn', '1', '', 'Trigger hold if checkin at pickup library', 'YesNo');
