bfb588abeca9e20e198d14438934f59a1f6a46f0
xljoha <johan.larsson@ub.gu.se>
2018-04-26 11:01:28 +0200

    commit messages from merge (#92)
    * Hide syndetics cover images setting
    
    * made firstname and surname disabled
    
    * added redirect
    
    added box for logging in with cas
    
    removed static placement from footer
    
    added lang=en to url if english is the current lang
    
    added lang to template
    
    added language class to logo-div
    
    added correct arrow
    
    added translations
    
    removed all translation-files
    
    removed dependency on ub-logo.less
    
    added logo for english
    
    changed headers
    
    added span to protect div-content when translating using jscript
    
    added span for javascript translation
    
    changed link copy
    
    logotype svg
    
    added appname to template
    
    added eng-translation as this is default in koha
    
    added hiddenfields to template based on modification. Default was create in this case
    
    removed credit and koha link in footer
    
    replace h2 and h3 with h1
    
    removed if-constrains on showbarcode based on user setting
    
    removed tables and added simple paragraph on fines-tab. Also addes SEK as unit
    
    removed account sum
    
    only showing rows that has amountoutstanding > 0
    
    added and removed columns from ovedue section
    
    removed number of renewals left info when renewal failed
    
    removed condition for loggedin for seeing the lang-toggler in header
    
    removed column for expires
    
    removed desc from desccolumn
    
    removed copy about charges and the thing about selecting provider
    
    removed code that removes timestamp hour:minute
    
    now differnt status if in que and ordered
    
    removed email input-field
    
    removed ical link
    
    added extra check when hiding login-link on auth page
    
    added span for jscript translation purpose
    
    copy fix
    
    changed copy for status (#77)
    
    added syspref for enabling and disabling search and external links in opac
    
    used enableOpacCatalogBrowsing to hide the search in opac if disabled
    
    added the external url to template if catalog searching and linking is disabled and linkToExternalSite is true
    
    used enableOpacCatalogBrowsing to hide the search in opac if disabled
    
    added the external url to template if catalog searching and linking is disabled and linkToExternalSite is true
    
    added syspref
    
    change is_cancelable to false when syspref is set

---
+++ installer/data/mysql/atomicupdate/enableSyndeticsCoverImagesInOpac.sql
@@ -0,0 +1,3 @@
+
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('enableCoverImagesInOpacList', '1', '', 'Show cover images in opac list', 'YesNo');
