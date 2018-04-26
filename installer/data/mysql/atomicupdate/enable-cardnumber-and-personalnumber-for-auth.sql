9823790e2afb048e46e75665027aea64fefda681
xljoha <johan.larsson@ub.gu.se>
2018-04-26 11:17:25 +0200

    Gub opac patches (#96)
    * now using cardnumber and personalnumber for login
    
    now using personalnumer as password and cardnumber as userid
    
    added translations for login inputs
    
    * removed sysprefs already in other branch
    
    * now first check for peronalnumber if that is not ok fallback to default koha behaviour
    
    * Hide syndetics cover images setting
    
    * made firstname and surname disabled
    
    * added redirect
    
    added box for logging in with cas
    
    removed static placement from footer
    
    added lang=en to url if english is the current lang
    
    added lang to template
    
    added language class to logo-div
    
    added correct arrow
    
    added translations
    
    removed all translation-files
    
    removed dependency on ub-logo.less
    
    added logo for english
    
    changed headers
    
    added span to protect div-content when translating using jscript
    
    added span for javascript translation
    
    changed link copy
    
    logotype svg
    
    added appname to template
    
    added eng-translation as this is default in koha
    
    added hiddenfields to template based on modification. Default was create in this case
    
    removed credit and koha link in footer
    
    replace h2 and h3 with h1
    
    removed if-constrains on showbarcode based on user setting
    
    removed tables and added simple paragraph on fines-tab. Also addes SEK as unit
    
    removed account sum
    
    only showing rows that has amountoutstanding > 0
    
    added and removed columns from ovedue section
    
    removed number of renewals left info when renewal failed
    
    removed condition for loggedin for seeing the lang-toggler in header
    
    removed column for expires
    
    removed desc from desccolumn
    
    removed copy about charges and the thing about selecting provider
    
    removed code that removes timestamp hour:minute
    
    now differnt status if in que and ordered
    
    removed email input-field
    
    removed ical link
    
    added extra check when hiding login-link on auth page
    
    added span for jscript translation purpose
    
    copy fix
    
    changed copy for status (#77)
    
    added syspref for enabling and disabling search and external links in opac
    
    used enableOpacCatalogBrowsing to hide the search in opac if disabled
    
    added the external url to template if catalog searching and linking is disabled and linkToExternalSite is true
    
    used enableOpacCatalogBrowsing to hide the search in opac if disabled
    
    added the external url to template if catalog searching and linking is disabled and linkToExternalSite is true
    
    added syspref
    
    change is_cancelable to false when syspref is set

---
+++ installer/data/mysql/atomicupdate/enable-cardnumber-and-personalnumber-for-auth.sql
@@ -0,0 +1,3 @@
+
+INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
+('enableCardnumberAndPersonalNumberAuth', '0', null, 'Toggle simple form of messaging options', 'YesNo');
