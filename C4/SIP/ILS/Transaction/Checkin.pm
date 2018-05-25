db358d1f9ae0cd2d258ff70c1f7264e0bd0af08a
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-05-25 14:00:48 +0200

    Do not backdate returns via SIP

--- C4/SIP/ILS/Transaction/Checkin.pm
+++ C4/SIP/ILS/Transaction/Checkin.pm
@@ -69,7 +69,7 @@ sub do_checkin {
                    . substr( $return_date, 16, 2 );
 
     $debug and warn "do_checkin() calling AddReturn($barcode, $branch)";
-    my ($return, $messages, $issue, $borrower) = AddReturn($barcode, $branch, undef, dt_from_string($return_date));
+    my ($return, $messages, $issue, $borrower) = AddReturn($barcode, $branch, undef, undef);
 
     if ( $checked_in_ok ) {
         delete $messages->{NotIssued};
