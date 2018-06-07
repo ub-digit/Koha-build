3d1eece2c7164132165259b737c5c516314390ec
Per Larsson <per.larsson@ub.gu.se>
2018-06-07 16:15:39 +0200

    Fines can be counted from a specific date

--- misc/cronjobs/fines.pl
+++ misc/cronjobs/fines.pl
@@ -48,6 +48,7 @@ my $verbose;
 my $output_dir;
 my $log;
 my $maxdays;
+my $from_date;
 
 GetOptions(
     'h|help'    => \$help,
@@ -55,6 +56,7 @@ GetOptions(
     'l|log'     => \$log,
     'o|out:s'   => \$output_dir,
     'm|maxdays:i' => \$maxdays,
+    'd|from-date:s' => \$from_date,
 );
 my $usage = << 'ENDUSAGE';
 
@@ -132,12 +134,22 @@ for my $overdue ( @{$overdues} ) {
       : ( $control eq 'PatronLibrary' )   ? $patron->branchcode
       :                                     $overdue->{branchcode};
 
+    unless ($branchcode) {
+        next;
+    }
+
 # In final case, CircControl must be PickupLibrary. (branchcode comes from issues table here).
     if ( !exists $is_holiday{$branchcode} ) {
         $is_holiday{$branchcode} = set_holiday( $branchcode, $today );
     }
 
-    my $datedue = dt_from_string( $overdue->{date_due} );
+    my $datedue;
+    if ($from_date and dt_from_string($from_date) > dt_from_string($overdue->{date_due})) {
+      $datedue = dt_from_string( $from_date );
+    }
+    else {
+      $datedue = dt_from_string( $overdue->{date_due} );
+    }
     if ( DateTime->compare( $datedue, $today ) == 1 ) {
         next;    # not overdue
     }
