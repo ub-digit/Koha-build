65c528e8b2562776ee907bcb34acd9d34d74ac73
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-06-13 11:39:39 +0200

    Fixed typo

--- installer/data/mysql/atomicupdate/opac-simplified-messaging.sql
+++ installer/data/mysql/atomicupdate/opac-simplified-messaging.sql
@@ -1,4 +1,4 @@
 INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
 ('enableSimpleOpacMessaging', '0', null, 'Toggle simple form of messaging options', 'YesNo');
 INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
-('whichActionsToTickUsingSimpleOpacMessaging','Advance_Notice|Hold_Filled|Overdue1|Overdue2',NULL,'A list of which messaging to set ','free'),
+('whichActionsToTickUsingSimpleOpacMessaging','Advance_Notice|Hold_Filled|Overdue1|Overdue2',NULL,'A list of which messaging to set ','free');
