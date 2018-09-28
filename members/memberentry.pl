8452b646771ee31bc4b0b41f2c531940f5ed176f
David Gustafsson <david.gustafsson@ub.gu.se>
2018-09-28 16:38:10 +0200

    Remove custom GUB properties before store

--- members/memberentry.pl
+++ members/memberentry.pl
@@ -479,6 +479,8 @@ if ( $op eq 'insert' || $op eq 'save') {
               }
           }
       }
+      delete $newdata{"opacmessaging-simple-radios"};
+      delete $newdata{"simple-messaging"};
     }
 }
 
