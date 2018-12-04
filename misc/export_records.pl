ub-digit/Koha

Bug 20485: Don't account for items timestamps when items not included
If combining "--dont_export_items" and "--date" options
in export_records.pl, don't include items modification/deletion
timestamp in biblio query conditions.

How to test:
1) Delete or modify a biblio item.
2) Run export_records.pl with "--dont_export_items" and "--date"
   options with a date value before item was deleted or modified,
   but after the biblio was modified.
3) The biblio for which an item was changed should not be included in
   the export.

Sponsored-by: Gothenburg University Library

misc/export_records.pl
@@ -102,21 +102,31 @@ $timestamp = ($timestamp) ? output_pref({ dt => dt_from_string($timestamp), date
 
 if ( $record_type eq 'bibs' ) {
     if ( $timestamp ) {
-        push @record_ids, $_->{biblionumber} for @{
-            $dbh->selectall_arrayref(q| (
-                SELECT biblionumber
-                FROM biblioitems
-                  LEFT JOIN items USING(biblionumber)
-                WHERE biblioitems.timestamp >= ?
-                  OR items.timestamp >= ?
-            ) UNION (
-                SELECT biblionumber
-                FROM biblioitems
-                  LEFT JOIN deleteditems USING(biblionumber)
-                WHERE biblioitems.timestamp >= ?
-                  OR deleteditems.timestamp >= ?
-            ) |, { Slice => {} }, ( $timestamp ) x 4 );
-        };
+        if ($dont_export_items) {
+            push @record_ids, $_->{biblionumber} for @{
+                $dbh->selectall_arrayref(q| (
+                    SELECT biblionumber
+                    FROM biblioitems
+                        LEFT JOIN items USING(biblionumber)
+                    WHERE biblioitems.timestamp >= ?
+                        OR items.timestamp >= ?
+                    ) UNION (
+                    SELECT biblionumber
+                    FROM biblioitems
+                        LEFT JOIN deleteditems USING(biblionumber)
+                    WHERE biblioitems.timestamp >= ?
+                        OR deleteditems.timestamp >= ?
+                ) |, { Slice => {} }, ( $timestamp ) x 4 );
+            };
+        } else {
+            push @record_ids, $_->{biblionumber} for @{
+                $dbh->selectall_arrayref(q| (
+                    SELECT biblionumber
+                    FROM biblioitems
+                    WHERE biblioitems.timestamp >= ?
+                ) |, { Slice => {} }, $timestamp );
+            };
+        }
     } else {
         my $conditions = {
             ( $starting_biblionumber or $ending_biblionumber )

ub-digit/Koha-build

misc/export_records.pl
@@ -142,21 +142,31 @@ $timestamp = ($timestamp) ? output_pref({ dt => dt_from_string($timestamp), date
 
 if ( $record_type eq 'bibs' ) {
     if ( $timestamp ) {
-        push @record_ids, $_->{biblionumber} for @{
-            $dbh->selectall_arrayref(q| (
-                SELECT biblio_metadata.biblionumber
-                FROM biblio_metadata
-                  LEFT JOIN items USING(biblionumber)
-                WHERE biblio_metadata.timestamp >= ?
-                  OR items.timestamp >= ?
-            ) UNION (
-                SELECT biblio_metadata.biblionumber
-                FROM biblio_metadata
-                  LEFT JOIN deleteditems USING(biblionumber)
-                WHERE biblio_metadata.timestamp >= ?
-                  OR deleteditems.timestamp >= ?
-            ) |, { Slice => {} }, ( $timestamp ) x 4 );
-        };
+        if ($dont_export_items) {
+            push @record_ids, $_->{biblionumber} for @{
+                $dbh->selectall_arrayref(q| (
+                    SELECT biblio_metadata.biblionumber
+                    FROM biblio_metadata
+                      LEFT JOIN items USING(biblionumber)
+                    WHERE biblio_metadata.timestamp >= ?
+                      OR items.timestamp >= ?
+                ) UNION (
+                    SELECT biblio_metadata.biblionumber
+                    FROM biblio_metadata
+                      LEFT JOIN deleteditems USING(biblionumber)
+                    WHERE biblio_metadata.timestamp >= ?
+                      OR deleteditems.timestamp >= ?
+                ) |, { Slice => {} }, ( $timestamp ) x 4 );
+            };
+        } else {
+            push @record_ids, $_->{biblionumber} for @{
+                $dbh->selectall_arrayref(q| (
+                    SELECT biblio_metadata.biblionumber
+                    FROM biblio_metadata
+                    WHERE biblio_metadata.timestamp >= ?
+                ) |, { Slice => {} }, $timestamp );
+            };
+        }
     } else {
         my $conditions = {
             ( $starting_biblionumber or $ending_biblionumber )