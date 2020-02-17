a0c97f74b62effe085a8b41166bf83f2c9a82be0
David Gustafsson <david.gustafsson@ub.gu.se>
2020-02-17 14:00:30 +0100

    Hide notice in staff client if older than 3 months

--- C4/Letters.pm
+++ C4/Letters.pm
@@ -1077,6 +1077,9 @@ ENDSQL
 
     my @query_params;
     my @whereclauses;
+
+    push @whereclauses, " time_queued > (NOW() - INTERVAL 3 MONTH) ";
+
     if ( exists $params->{'borrowernumber'} ) {
         push @whereclauses, ' borrowernumber = ? ';
         push @query_params, $params->{'borrowernumber'};
