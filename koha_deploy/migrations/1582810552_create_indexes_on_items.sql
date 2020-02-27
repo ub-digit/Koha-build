50427ca8a70bb04842e2089c02b52eba564c4d2e
Per Larsson <per.larsson@ub.gu.se>
2020-02-27 14:39:39 +0100

    Create indexes on items columns

---
+++ koha_deploy/migrations/1582810552_create_indexes_on_items.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_items_withdrawn ON items(withdrawn);
+CREATE INDEX idx_items_itemlost ON items(itemlost);
+CREATE INDEX idx_items_notforloan ON items(notforloan);
+CREATE INDEX idx_items_restricted ON items(restricted);
