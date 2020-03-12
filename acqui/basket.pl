fb4f789a5438ecc1b976ce15e52d6502fa3d3e1d
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2020-03-12 15:11:02 +0100

    Remove usage of actual cost when calculating total cost
    New code in 23522 uses actual cost instead of budgeted cost when calculating total, if actual cost exists. Unfortunately actual cost defaults to 0.00, which is considered to be "true" and therefor ignores budgeted cost. This removes the attempt to use actual cost for now, but should probably be replaced with an actual check for 0.00 instead.

--- acqui/basket.pl
+++ acqui/basket.pl
@@ -464,8 +464,8 @@ sub get_order_infos {
     # If we have an actual cost that should be the total, otherwise use the ecost
     $line{unitprice_tax_included} += 0;
     $line{unitprice_tax_excluded} += 0;
-    my $cost_tax_included = $line{unitprice_tax_included} || $line{ecost_tax_included};
-    my $cost_tax_excluded = $line{unitprice_tax_excluded} || $line{ecost_tax_excluded};
+    my $cost_tax_included = $line{ecost_tax_included};
+    my $cost_tax_excluded = $line{ecost_tax_excluded};
     $line{total_tax_included} = get_rounded_price($cost_tax_included) * $line{quantity};
     $line{total_tax_excluded} = get_rounded_price($cost_tax_excluded) * $line{quantity};
     $line{tax_value} = $line{tax_value_on_ordering};
