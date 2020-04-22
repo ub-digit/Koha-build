52cb8d60e7733836f1b00c31f4a79ffd9ab1cf4f
Johan <johan.larsson@ub.gu.se>
2020-04-22 15:51:41 +0200

    tab for barcode added in opac

---
+++ opac/opac-barcode.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+
+# This file is part of Koha.
+# parts copyright 2010 BibLibre
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
+
+
+use Modern::Perl;
+
+use CGI qw ( -utf8 );
+
+use C4::Auth;
+use C4::Koha;
+use C4::Members;
+use C4::Members::AttributeTypes;
+use C4::Members::Attributes qw/GetBorrowerAttributeValue/;
+use C4::Output;
+use Koha::DateUtils;
+use Koha::Patron::Attribute::Types;
+use Koha::Patrons;
+
+use constant ATTRIBUTE_SHOW_BARCODE => 'SHOW_BCODE';
+
+use Scalar::Util qw(looks_like_number);
+use Date::Calc qw(
+  Today
+  Add_Delta_Days
+  Date_to_Days
+);
+
+my $query = new CGI;
+
+# CAS single logout handling
+# Will print header and exit
+C4::Context->preference('casAuthentication') and C4::Auth_with_cas::logout_if_required($query);
+
+my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
+    {
+        template_name   => "opac-barcode.tt",
+        query           => $query,
+        type            => "opac",
+        authnotrequired => 0,
+        debug           => 1,
+    }
+);
+
+my %renewed = map { $_ => 1 } split( ':', $query->param('renewed') || '' );
+
+my $show_priority;
+for ( C4::Context->preference("OPACShowHoldQueueDetails") ) {
+    m/priority/ and $show_priority = 1;
+}
+
+my $patron = Koha::Patrons->find( $borrowernumber );
+my $borr = $patron->unblessed;
+$template->param( cardnumber => $borr->{cardnumber}, barcodeview => 1);
+
+output_html_with_http_headers $query, $cookie, $template->output, undef, { force_no_caching => 1 };
