203e8eaa66b6016a7c926593558d0b73482f8166
Johan <johan.larsson@ub.gu.se>
2020-09-14 11:24:31 +0200

    changes namne on config var

--- Koha/Template/Plugin/UBOnlinePayment.pm
+++ Koha/Template/Plugin/UBOnlinePayment.pm
@@ -8,7 +8,7 @@ sub Active {
     return C4::Context->config("online_payments");
 }
 sub URL {
-    return C4::Context->config("online_payments_portal_url");
+    return C4::Context->config("online_payment_portal_url");
 }
 
 1;
