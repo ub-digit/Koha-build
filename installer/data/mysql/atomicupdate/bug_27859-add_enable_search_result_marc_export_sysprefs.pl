6658736270aa44f3739371c8c18ada83645dde82
David Gustafsson <david.gustafsson@ub.gu.se>
2020-09-25 16:07:41 +0200

    Bug 27859: Export staff interface search results when using Elasticsearch
    Enable export of staff interface search results in different marc formats.
    This feature is only supported when using Elasticsearch.
    
    To test:
    1) Apply patch
    2) Run installer/data/mysql/updatedatabase.pl
    3) Make sure the EnableElasticsearchSearchResultExport system
       preference is enabled
    4) Make sure the current user has the export_catalog permission
       (Use all tools > Export bibliographic and holdings data)
    5) Perform a search
    6) Export the serach result by choosing a format under the
       "Export all results" drop down
    7) Verify that link(s) with exported data appear on the
       background job detail page when the export is completed
    8) Revoke the permission in step 4 and ensure exporting
       is no longer possible
    9) Run tests in t/db_dependent/Koha/SearchEngine/Elasticsearch.t
    
    Sponsored-by: Gothenburg University Library

---
+++ installer/data/mysql/atomicupdate/bug_27859-add_enable_search_result_marc_export_sysprefs.pl
@@ -0,0 +1,19 @@
+use Modern::Perl;
+
+return {
+    bug_number => "27859",
+    description => "Add system preferences",
+    up => sub {
+        my ($args) = @_;
+        my ($dbh, $out) = @$args{qw(dbh out)};
+
+        $dbh->do(q{ INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('EnableElasticsearchSearchResultExport', 1, NULL, 'Enable search result export', 'YesNo') });
+        say $out "Added new system preference 'EnableElasticsearchSearchResultExport'";
+
+        $dbh->do(q{ INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('ElasticsearchSearchResultExportCustomFormats', '', NULL, 'Search result export custom formats', 'textarea') });
+        say $out "Added new system preference 'ElasticsearchSearchResultExportCustomFormats'";
+
+        $dbh->do(q{ INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('ElasticsearchSearchResultExportLimit', NULL, NULL, 'Search result export limit', 'integer') });
+        say $out "Added new system preference 'ElasticsearchSearchResultExportLimit'";
+    },
+}
