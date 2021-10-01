0f5d7c35884114b5e94f7ddcaecfa365b4ec646a
David Gustafsson <glasklas@gmail.com>
2021-10-01 16:11:50 +0200

    Bug 14957: Fix imports in marc-overlay-rules.pl

--- admin/marc-overlay-rules.pl
+++ admin/marc-overlay-rules.pl
@@ -21,8 +21,8 @@ use CGI qw ( -utf8 );
 use Try::Tiny;
 
 use C4::Context;
-use C4::Auth;
-use C4::Output;
+use C4::Auth qw( get_template_and_user );
+use C4::Output qw( output_html_with_http_headers );
 use C4::ImportBatch;
 use Koha::MarcOverlayRules;
 use Koha::Patron::Categories;
