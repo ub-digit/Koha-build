613cb4757c2f3a8a1aedc06ff29e24c895f7b41e
Per Larsson <per.larsson@ub.gu.se>
2021-10-04 09:38:50 +0200

    --amend

--- installer/data/mysql/atomicupdate/enable-cardnumber-and-personalnumber-for-auth.sql
+++
@@ -1,3 +0,0 @@
-
-INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
-('enableCardnumberAndPersonalNumberAuth', '0', null, 'Toggle simple form of messaging options', 'YesNo');
--- installer/data/mysql/atomicupdate/opac-redirect.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
-('OPACRedirect', '0', null, 'Redirect from homepage to userpage', 'YesNo');
