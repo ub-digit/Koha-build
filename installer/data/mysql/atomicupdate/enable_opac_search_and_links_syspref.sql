f458804ea2db10e186f79a5fd97deee42a3336b7
Per Larsson <per.larsson@ub.gu.se>
2021-10-04 09:38:50 +0200

    --amend

--- installer/data/mysql/atomicupdate/enable_opac_search_and_links_syspref.sql
+++
@@ -1,8 +0,0 @@
-INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
-('enableOpacCatalogBrowsing', '1', '', 'Toggle the search box visibility.', 'YesNo');
-
-INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
-('linkToExternalSite', '0', '', 'Links to external site', 'YesNo');
-
-INSERT IGNORE INTO systempreferences ( `variable`, `value`, `options`, `explanation`, `type`) VALUES
-('externalSiteURL', '', null, 'URL to external site', 'Free');
