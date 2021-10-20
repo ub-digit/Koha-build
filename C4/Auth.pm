0d4885f10965d819bafd76340bb5c3370d39aefa
Stefan Berndtsson <stefan.berndtsson@ub.gu.se>
2021-10-20 15:30:11 +0200

    Add sysprefs SIPPasswordFromAttribute and SIPPasswordAttribute to make it possible to use patron attribute instead of password when using SIP, but not for other authentications

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -75,7 +75,7 @@ BEGIN {
     @EXPORT_OK = qw(
         checkauth check_api_auth get_session check_cookie_auth checkpw checkpw_internal checkpw_hash
         get_all_subpermissions get_cataloguing_page_permissions get_user_subpermissions in_iprange
-        get_template_and_user haspermission create_basic_session
+        get_template_and_user haspermission create_basic_session checkattributepw
     );
 
     $ldap      = C4::Context->config('useldapserver') || 0;
@@ -2075,6 +2075,28 @@ sub checkpw_internal {
 
             C4::Context->set_userenv( $borrowernumber, $userid, $cardnumber,
                 $firstname, $surname, $branchcode, $branchname, $flags ) unless $no_set_userenv;
+            return 1, $cardnumber, $userid;
+        }
+    }
+    return 0;
+}
+
+sub checkattributepw {
+    my ( $userid, $password, $attribute ) = @_;
+
+    $password = Encode::encode( 'UTF-8', $password )
+      if Encode::is_utf8($password);
+
+    my $dbh = C4::Context->dbh;
+    my $sth =
+      $dbh->prepare(
+        "select ba.attribute,b.cardnumber from borrowers b join borrower_attributes ba on b.borrowernumber = ba.borrowernumber where ba.code = ? and b.userid = ?"
+      );
+    $sth->execute($attribute, $userid);
+    if ( $sth->rows ) {
+        my ( $attribute_value, $cardnumber ) = $sth->fetchrow;
+
+        if ( $password eq $attribute_value ) {
             return 1, $cardnumber, $userid;
         }
     }
