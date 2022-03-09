53adea30921cb739be33a32dd5460423e9de4175
Per Larsson <per.larsson@ub.gu.se>
2022-03-09 13:16:26 +0100

    Remove obsolete migrations

--- koha_deploy/migrations/1582810552_create_indexes_on_items.sql
+++
@@ -1,4 +0,0 @@
-CREATE INDEX idx_items_withdrawn ON items(withdrawn);
-CREATE INDEX idx_items_itemlost ON items(itemlost);
-CREATE INDEX idx_items_notforloan ON items(notforloan);
-CREATE INDEX idx_items_restricted ON items(restricted);
