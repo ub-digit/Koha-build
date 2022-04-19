eaaf6cf50101c7d5c3771c0ce03629bc1ccac2ed
Per Larsson <per.larsson@ub.gu.se>
2022-04-19 11:15:37 +0200

    Remove obsolete migration

--- koha_deploy/migrations/1582808035_gub-format-facet-order.sql
+++
@@ -1,3 +0,0 @@
-UPDATE search_field SET facet_order = facet_order + 1 WHERE facet_order IS NOT NULL;
-UPDATE search_field SET facet_order = 1 WHERE name = 'author';
-UPDATE search_field SET facet_order = 2 WHERE name = 'gub-format';
