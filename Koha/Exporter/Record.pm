ub-digit/Koha

Bug 20551: Add option for including deleted records in export_records.pl
Add option "--include_deleted" to include deleted
biblios in export_records.pl as marc records with
record status (in leader) set to "d" and
"--deleted_only" to export only deleted biblios.

How to test:
1) Run tests in t/db_dependent/Exporter/Record.t
2) All tests should pass
3) Delete a biblio record in Koha
4) Run the export script as:
   `export_records.pl --date=<date-just-before-deletion>
   --include_deleted`
5) Open the generated koha.mrc, and verify that contains
   the recently deleted record, and that the record header
   has record status "d".
6) Perform the same check with:
   `export_records.pl --date=<date-just-before-deletion>
      --include_deleted --format=xml`
7) Run:
   `export_records.pl --date=<date-just-before-deletion>
      --deleted_only --format=xml`
    and verify that only the deleted biblio was included
    in the export.

Sponsored-by: Gothenburg University Library

Koha/Exporter/Record.pm
@@ -6,9 +6,14 @@ use MARC::File::USMARC;
 
 use C4::AuthoritiesMarc;
 use C4::Biblio;
+use C4::Charset;
 use C4::Record;
 use Koha::CsvProfiles;
+use Koha::Database;
 use Koha::Logger;
+
+use MARC::Record;
+use MARC::File::XML;
 
 sub _get_record_for_export {
     my ($params)           = @_;
@@ -47,6 +52,38 @@ sub _get_record_for_export {
         }
     }
     C4::Biblio::RemoveAllNsb($record) if $clean;
+    return $record;
+}
+
+sub _get_deleted_biblio_for_export {
+    my ($params)           = @_;
+    my $record_id = $params->{record_id};
+    # Creating schema is expensive, allow caller to
+    # pass it so don't have to recreate for each call
+    my $resultset = $params->{resultset} || Koha::Database
+        ->new()
+        ->schema()
+        ->resultset('DeletedbiblioMetadata');
+    my $marc_flavour = C4::Context->preference('marcflavour');
+    my $biblio_metadata = $resultset->find({
+        'biblionumber' => $record_id,
+        'format' => 'marcxml',
+        'marcflavour' => $marc_flavour
+    });
+    my $marc_xml = $biblio_metadata->metadata;
+    $marc_xml = StripNonXmlChars($marc_xml);
+
+    my $record = eval {
+        MARC::Record::new_from_xml($marc_xml, 'UTF-8', $marc_flavour)
+    };
+    if ($@) {
+        warn "Failed to load MARCXML for biblio with biblionumber \"$record_id\": $@";
+        return undef;
+    }
+    # Set deleted flag (record status, position 05)
+    my $leader = $record->leader;
+    substr $leader, 5, 1, 'd';
+    $record->leader($leader);
     return $record;
 }
 
@@ -91,6 +128,7 @@ sub export {
 
     my $record_type        = $params->{record_type};
     my $record_ids         = $params->{record_ids} || [];
+    my $deleted_record_ids = $params->{deleted_record_ids} || [];
     my $format             = $params->{format};
     my $itemnumbers        = $params->{itemnumbers} || [];    # Does not make sense with record_type eq auths
     my $export_items       = $params->{export_items};
@@ -102,7 +140,7 @@ sub export {
         Koha::Logger->get->warn( "No record_type given." );
         return;
     }
-    return unless @$record_ids;
+    return unless (@{$record_ids} || @{$deleted_record_ids} && $format ne 'csv');
 
     my $fh;
     if ( $output_filepath ) {
@@ -113,42 +151,73 @@ sub export {
         binmode STDOUT, ':encoding(UTF-8)' unless $format eq 'csv';
     }
 
-    if ( $format eq 'iso2709' ) {
-        for my $record_id (@$record_ids) {
-            my $record = _get_record_for_export( { %$params, record_id => $record_id } );
-            my $errorcount_on_decode = eval { scalar( MARC::File::USMARC->decode( $record->as_usmarc )->warnings() ) };
-            if ( $errorcount_on_decode or $@ ) {
-                my $msg = "Record $record_id could not be exported. " .
-                    ( $@ // '' );
-                chomp $msg;
-                Koha::Logger->get->info( $msg );
-                next;
-            }
-            print $record->as_usmarc();
-        }
-    } elsif ( $format eq 'xml' ) {
-        my $marcflavour = C4::Context->preference("marcflavour");
-        MARC::File::XML->default_record_format( ( $marcflavour eq 'UNIMARC' && $record_type eq 'auths' ) ? 'UNIMARCAUTH' : $marcflavour );
+    if ($format eq 'xml' || $format eq 'iso2709') {
+        my @records;
+        @records = map {
+            my $record = _get_record_for_export({ %{$params}, record_id => $_ });
+            Koha::Logger->get->warn( "Record $_ could not be loaded." ) unless $record;
+            $record ? $record : ();
+        } @{$record_ids};
 
-        print MARC::File::XML::header();
-        print "\n";
-        for my $record_id (@$record_ids) {
-            my $record = _get_record_for_export( { %$params, record_id => $record_id } );
-            if( !$record ) {
-                Koha::Logger->get->info( "Record $record_id could not be exported." );
-                next;
+        my @deleted_records;
+        if (@{$deleted_record_ids}) {
+            my $resultset = Koha::Database
+            ->new()
+            ->schema()
+            ->resultset('DeletedbiblioMetadata');
+            @deleted_records = map {
+                my $record = _get_deleted_biblio_for_export({
+                    record_id => $_,
+                    resultset => $resultset,
+                });
+                Koha::Logger->get->warn( "Deleted record $_ could not be loaded." ) unless $record;
+                $record ? $record : ();
+            } @{$deleted_record_ids};
+        }
+        if ( $format eq 'iso2709' ) {
+            my $encoding_validator = sub {
+                my ($record_type) = @_;
+                return sub {
+                    my ($record) = @_;
+                    my $errorcount_on_decode = eval { scalar(MARC::File::USMARC->decode($record->as_usmarc)->warnings()) };
+                    if ($errorcount_on_decode || $@) {
+                        my ($id_tag, $id_subfield) = GetMarcFromKohaField('biblio.biblionumber', '');
+                        my $record_id = $record->subfield($id_tag, $id_subfield);
+                        my $msg = "$record_type $record_id could not be USMARC decoded/encoded. " . ($@ // '');
+                        chomp $msg;
+                        Koha::Logger->get->warn($msg);
+                        return 0;
+                    }
+                    return 1;
+                }
+            };
+            my $validator = $encoding_validator->('Record');
+            for my $record (grep { $validator->($_) } @records) {
+                print $record->as_usmarc();
             }
-            print MARC::File::XML::record($record);
+            if (@deleted_records) {
+                $validator = $encoding_validator->('Deleted record');
+                for my $deleted_record (grep { $validator->($_) } @deleted_records) {
+                    print $deleted_record->as_usmarc();
+                }
+            }
+        } elsif ( $format eq 'xml' ) {
+            my $marcflavour = C4::Context->preference("marcflavour");
+            MARC::File::XML->default_record_format( ( $marcflavour eq 'UNIMARC' && $record_type eq 'auths' ) ? 'UNIMARCAUTH' : $marcflavour );
+            print MARC::File::XML::header();
+            print "\n";
+            for my $record (@records, @deleted_records) {
+                print MARC::File::XML::record($record);
+                print "\n";
+            }
+            print MARC::File::XML::footer();
             print "\n";
         }
-        print MARC::File::XML::footer();
-        print "\n";
     } elsif ( $format eq 'csv' ) {
         die 'There is no valid csv profile defined for this export'
             unless Koha::CsvProfiles->find( $csv_profile_id );
         print marc2csv( $record_ids, $csv_profile_id, $itemnumbers );
     }
-
     close $fh if $output_filepath;
 }
 

misc/export_records.pl
@@ -32,10 +32,36 @@ use Koha::CsvProfiles;
 use Koha::Exporter::Record;
 use Koha::DateUtils qw( dt_from_string output_pref );
 
-my ( $output_format, $timestamp, $dont_export_items, $csv_profile_id, $deleted_barcodes, $clean, $filename, $record_type, $id_list_file, $starting_authid, $ending_authid, $authtype, $starting_biblionumber, $ending_biblionumber, $itemtype, $starting_callnumber, $ending_callnumber, $start_accession, $end_accession, $help );
+my (
+    $output_format,
+    $timestamp,
+    $include_deleted,
+    $deleted_only,
+    $dont_export_items,
+    $csv_profile_id,
+    $deleted_barcodes,
+    $clean,
+    $filename,
+    $record_type,
+    $id_list_file,
+    $starting_authid,
+    $ending_authid,
+    $authtype,
+    $starting_biblionumber,
+    $ending_biblionumber,
+    $itemtype,
+    $starting_callnumber,
+    $ending_callnumber,
+    $start_accession,
+    $end_accession,
+    $help
+);
+
 GetOptions(
     'format=s'                => \$output_format,
     'date=s'                  => \$timestamp,
+    'include_deleted'         => \$include_deleted,
+    'deleted_only'            => \$deleted_only,
     'dont_export_items'       => \$dont_export_items,
     'csv_profile_id=s'        => \$csv_profile_id,
     'deleted_barcodes'        => \$deleted_barcodes,
@@ -67,6 +93,18 @@ $record_type ||= 'bibs';
 # Retrocompatibility for the format parameter
 $output_format = 'iso2709' if $output_format eq 'marc';
 
+if ($include_deleted || $deleted_only) {
+   if ($record_type ne 'bibs') {
+        pod2usage(q|Option "--include_deleted" or "--deleted_only" can only be used with "--record-type=bibs"|);
+    }
+    if (!$timestamp) {
+        pod2usage(q|Option "--include_deleted" or "--deleted_only" requires that "--date" is also set|);
+    }
+    if ($output_format eq 'csv') {
+        pod2usage(q|Option "--include_deleted" or "--deleted_only" cannot be used with "--format=csv"|);
+    }
+}
+
 if ( $output_format eq 'csv' and $record_type eq 'auths' ) {
     pod2usage(q|CSV output is only available for biblio records|);
 }
@@ -97,26 +135,38 @@ open STDOUT, '>', $filename if $filename;
 
 
 my @record_ids;
+my @deleted_record_ids;
 
 $timestamp = ($timestamp) ? output_pref({ dt => dt_from_string($timestamp), dateformat => 'iso', dateonly => 0, }): '';
 
 if ( $record_type eq 'bibs' ) {
     if ( $timestamp ) {
-        push @record_ids, $_->{biblionumber} for @{
-            $dbh->selectall_arrayref(q| (
-                SELECT biblionumber
-                FROM biblioitems
-                  LEFT JOIN items USING(biblionumber)
-                WHERE biblioitems.timestamp >= ?
-                  OR items.timestamp >= ?
-            ) UNION (
-                SELECT biblionumber
-                FROM biblioitems
-                  LEFT JOIN deleteditems USING(biblionumber)
-                WHERE biblioitems.timestamp >= ?
-                  OR deleteditems.timestamp >= ?
-            ) |, { Slice => {} }, ( $timestamp ) x 4 );
-        };
+        unless ($deleted_only) {
+            push @record_ids, $_->{biblionumber} for @{
+                $dbh->selectall_arrayref(q| (
+                    SELECT biblionumber
+                    FROM biblioitems
+                      LEFT JOIN items USING(biblionumber)
+                    WHERE biblioitems.timestamp >= ?
+                      OR items.timestamp >= ?
+                ) UNION (
+                    SELECT biblionumber
+                    FROM biblioitems
+                      LEFT JOIN deleteditems USING(biblionumber)
+                    WHERE biblioitems.timestamp >= ?
+                      OR deleteditems.timestamp >= ?
+                ) |, { Slice => {} }, ( $timestamp ) x 4 );
+            };
+        }
+        if ($include_deleted || $deleted_only) {
+            push @deleted_record_ids, $_->{biblionumber} for @{
+                $dbh->selectall_arrayref(q|
+                    SELECT `biblionumber`
+                    FROM `deletedbiblio`
+                    WHERE `timestamp` >= ?
+                |, { Slice => {} }, $timestamp);
+            };
+        }
     } else {
         my $conditions = {
             ( $starting_biblionumber or $ending_biblionumber )
@@ -199,6 +249,7 @@ else {
     Koha::Exporter::Record::export(
         {   record_type        => $record_type,
             record_ids         => \@record_ids,
+            deleted_record_ids => \@deleted_record_ids,
             format             => $output_format,
             csv_profile_id     => $csv_profile_id,
             export_items       => (not $dont_export_items),
@@ -215,7 +266,7 @@ export records - This script exports record (biblios or authorities)
 
 =head1 SYNOPSIS
 
-export_records.pl [-h|--help] [--format=format] [--date=datetime] [--record-type=TYPE] [--dont_export_items] [--deleted_barcodes] [--clean] [--id_list_file=PATH] --filename=outputfile
+export_records.pl [-h|--help] [--format=format] [--date=datetime] [--include_deleted] [--deleted_only] [--record-type=TYPE] [--dont_export_items] [--deleted_barcodes] [--clean] [--id_list_file=PATH] --filename=outputfile
 
 =head1 OPTIONS
 
@@ -235,6 +286,16 @@ Print a brief help message.
                         set (dd/mm/yyyy[ hh:mm:ss] for metric, yyyy-mm-dd[ hh:mm:ss] for iso,
                         mm/dd/yyyy[ hh:mm:ss] for us) records exported are the ones that
                         have been modified since DATETIME.
+
+=item B<--include_deleted>
+
+ --include_deleted      If enabled, when using --date option, deleted records will be included in export as marc records
+                        with leader record status set to "d" (deleted).
+
+=item B<--include_deleted>
+
+ --include_deleted      If enabled, when using --date option, only deleted records will be included in export as marc
+                        records with leader record status set to "d" (deleted).
 
 =item B<--record-type>
 

ub-digit/Koha-build

Koha/Exporter/Record.pm
@@ -6,10 +6,15 @@ use MARC::File::USMARC;
 
 use C4::AuthoritiesMarc;
 use C4::Biblio qw( GetMarcFromKohaField );
+use C4::Charset;
 use C4::Record;
 use Koha::CsvProfiles;
+use Koha::Database;
 use Koha::Logger;
 use List::Util qw( all any );
+
+use MARC::Record;
+use MARC::File::XML;
 
 sub _get_record_for_export {
     my ($params)           = @_;
@@ -105,6 +110,40 @@ sub _get_record_for_export {
     return $record;
 }
 
+sub _get_deleted_biblio_for_export {
+    my ($params)           = @_;
+    my $biblionumber = $params->{biblionumber};
+    # Creating schema is expensive, allow caller to
+    # pass it so don't have to recreate for each call
+    my $resultset = $params->{resultset} || Koha::Database
+        ->new()
+        ->schema()
+        ->resultset('DeletedbiblioMetadata');
+    my $marc_flavour = C4::Context->preference('marcflavour');
+    my $biblio_metadata = $resultset->find({
+        'biblionumber' => $biblionumber,
+        'format' => 'marcxml',
+        'marcflavour' => $marc_flavour
+    });
+    my $marc_xml = $biblio_metadata->metadata;
+    $marc_xml = StripNonXmlChars($marc_xml);
+
+    my $record = eval {
+        MARC::Record::new_from_xml($marc_xml, 'UTF-8', $marc_flavour)
+    };
+    if (!$record) {
+        Koha::Logger->get->warn(
+            "Failed to load MARCXML for deleted biblio with biblionumber \"$biblionumber\": $@"
+        );
+        return;
+    }
+    # Set deleted flag (record status, position 05)
+    my $leader = $record->leader;
+    substr $leader, 5, 1, 'd';
+    $record->leader($leader);
+    return $record;
+}
+
 sub _get_authority_for_export {
     my ($params) = @_;
     my $authid = $params->{authid} || return;
@@ -122,7 +161,12 @@ sub _get_biblio_for_export {
 
     my $record = eval { C4::Biblio::GetMarcBiblio({ biblionumber => $biblionumber }); };
 
-    return if $@ or not defined $record;
+    if (!$record) {
+        Koha::Logger->get->warn(
+            "Failed to load MARCXML for biblio with biblionumber \"$biblionumber\": $@"
+        );
+        return;
+    }
 
     if ($export_items) {
         C4::Biblio::EmbedItemsInMarcBiblio({
@@ -149,6 +193,7 @@ sub export {
 
     my $record_type        = $params->{record_type};
     my $record_ids         = $params->{record_ids} || [];
+    my $deleted_record_ids = $params->{deleted_record_ids} || [];
     my $format             = $params->{format};
     my $itemnumbers        = $params->{itemnumbers} || [];    # Does not make sense with record_type eq auths
     my $export_items       = $params->{export_items};
@@ -160,7 +205,7 @@ sub export {
         Koha::Logger->get->warn( "No record_type given." );
         return;
     }
-    return unless @$record_ids;
+    return unless (@{$record_ids} || @{$deleted_record_ids} && $format ne 'csv');
 
     my $fh;
     if ( $output_filepath ) {
@@ -171,40 +216,71 @@ sub export {
         binmode STDOUT, ':encoding(UTF-8)' unless $format eq 'csv';
     }
 
-    if ( $format eq 'iso2709' ) {
-        for my $record_id (@$record_ids) {
-            my $record = _get_record_for_export( { %$params, record_id => $record_id } );
-            next unless $record;
-            my $errorcount_on_decode = eval { scalar( MARC::File::USMARC->decode( $record->as_usmarc )->warnings() ) };
-            if ( $errorcount_on_decode or $@ ) {
-                my $msg = "Record $record_id could not be exported. " .
-                    ( $@ // '' );
-                chomp $msg;
-                Koha::Logger->get->info( $msg );
-                next;
-            }
-            print $record->as_usmarc();
-        }
-    } elsif ( $format eq 'xml' ) {
-        my $marcflavour = C4::Context->preference("marcflavour");
-        MARC::File::XML->default_record_format( ( $marcflavour eq 'UNIMARC' && $record_type eq 'auths' ) ? 'UNIMARCAUTH' : $marcflavour );
+    if ($format eq 'xml' || $format eq 'iso2709') {
+        my @records;
+        @records = map {
+            my $record = _get_record_for_export({ %{$params}, record_id => $_ });
+            $record ? $record : ();
+        } @{$record_ids};
 
-        print MARC::File::XML::header();
-        print "\n";
-        for my $record_id (@$record_ids) {
-            my $record = _get_record_for_export( { %$params, record_id => $record_id } );
-            next unless $record;
-            print MARC::File::XML::record($record);
+        my @deleted_records;
+        if (@{$deleted_record_ids}) {
+            my $resultset = Koha::Database
+            ->new()
+            ->schema()
+            ->resultset('DeletedbiblioMetadata');
+            @deleted_records = map {
+                my $record = _get_deleted_biblio_for_export({
+                    biblionumber => $_,
+                    resultset => $resultset,
+                });
+                $record ? $record : ();
+            } @{$deleted_record_ids};
+        }
+        if ( $format eq 'iso2709' ) {
+            my $encoding_validator = sub {
+                my ($record_type) = @_;
+                return sub {
+                    my ($record) = @_;
+                    my $errorcount_on_decode = eval { scalar(MARC::File::USMARC->decode($record->as_usmarc)->warnings()) };
+                    if ($errorcount_on_decode || $@) {
+                        my ($id_tag, $id_subfield) = GetMarcFromKohaField('biblio.biblionumber', '');
+                        my $record_id = $record->subfield($id_tag, $id_subfield);
+                        my $msg = "$record_type $record_id could not be USMARC decoded/encoded. " . ($@ // '');
+                        chomp $msg;
+                        Koha::Logger->get->warn($msg);
+                        return 0;
+                    }
+                    return 1;
+                }
+            };
+            my $validator = $encoding_validator->('Record');
+            for my $record (grep { $validator->($_) } @records) {
+                print $record->as_usmarc();
+            }
+            if (@deleted_records) {
+                $validator = $encoding_validator->('Deleted record');
+                for my $deleted_record (grep { $validator->($_) } @deleted_records) {
+                    print $deleted_record->as_usmarc();
+                }
+            }
+        } elsif ( $format eq 'xml' ) {
+            my $marcflavour = C4::Context->preference("marcflavour");
+            MARC::File::XML->default_record_format( ( $marcflavour eq 'UNIMARC' && $record_type eq 'auths' ) ? 'UNIMARCAUTH' : $marcflavour );
+            print MARC::File::XML::header();
+            print "\n";
+            for my $record (@records, @deleted_records) {
+                print MARC::File::XML::record($record);
+                print "\n";
+            }
+            print MARC::File::XML::footer();
             print "\n";
         }
-        print MARC::File::XML::footer();
-        print "\n";
     } elsif ( $format eq 'csv' ) {
         die 'There is no valid csv profile defined for this export'
             unless Koha::CsvProfiles->find( $csv_profile_id );
         print marc2csv( $record_ids, $csv_profile_id, $itemnumbers );
     }
-
     close $fh if $output_filepath;
 }
 

misc/export_records.pl
@@ -36,6 +36,8 @@ use Koha::DateUtils qw( dt_from_string output_pref );
 my (
     $output_format,
     $timestamp,
+    $include_deleted,
+    $deleted_only,
     $dont_export_items,
     $csv_profile_id,
     $deleted_barcodes,
@@ -60,6 +62,8 @@ my (
 GetOptions(
     'format=s'                => \$output_format,
     'date=s'                  => \$timestamp,
+    'include_deleted'         => \$include_deleted,
+    'deleted_only'            => \$deleted_only,
     'dont_export_items'       => \$dont_export_items,
     'csv_profile_id=s'        => \$csv_profile_id,
     'deleted_barcodes'        => \$deleted_barcodes,
@@ -91,6 +95,18 @@ $record_type ||= 'bibs';
 
 # Retrocompatibility for the format parameter
 $output_format = 'iso2709' if $output_format eq 'marc';
+
+if ($include_deleted || $deleted_only) {
+   if ($record_type ne 'bibs') {
+        pod2usage(q|Option "--include_deleted" or "--deleted_only" can only be used with "--record-type=bibs"|);
+    }
+    if (!$timestamp) {
+        pod2usage(q|Option "--include_deleted" or "--deleted_only" requires that "--date" is also set|);
+    }
+    if ($output_format eq 'csv') {
+        pod2usage(q|Option "--include_deleted" or "--deleted_only" cannot be used with "--format=csv"|);
+    }
+}
 
 if ( $output_format eq 'csv' and $record_type eq 'auths' ) {
     pod2usage(q|CSV output is only available for biblio records|);
@@ -138,34 +154,46 @@ open STDOUT, '>', $filename if $filename;
 
 
 my @record_ids;
+my @deleted_record_ids;
 
 $timestamp = ($timestamp) ? output_pref({ dt => dt_from_string($timestamp), dateformat => 'iso', dateonly => 0, }): '';
 
 if ( $record_type eq 'bibs' ) {
     if ( $timestamp ) {
-        if (!$dont_export_items) {
-            push @record_ids, $_->{biblionumber} for @{
-                $dbh->selectall_arrayref(q| (
-                    SELECT biblio_metadata.biblionumber
-                    FROM biblio_metadata
-                      LEFT JOIN items USING(biblionumber)
-                    WHERE biblio_metadata.timestamp >= ?
-                      OR items.timestamp >= ?
-                ) UNION (
-                    SELECT biblio_metadata.biblionumber
-                    FROM biblio_metadata
-                      LEFT JOIN deleteditems USING(biblionumber)
-                    WHERE biblio_metadata.timestamp >= ?
-                      OR deleteditems.timestamp >= ?
-                ) |, { Slice => {} }, ( $timestamp ) x 4 );
-            };
-        } else {
-            push @record_ids, $_->{biblionumber} for @{
-                $dbh->selectall_arrayref(q| (
-                    SELECT biblio_metadata.biblionumber
-                    FROM biblio_metadata
-                    WHERE biblio_metadata.timestamp >= ?
-                ) |, { Slice => {} }, $timestamp );
+        unless ($deleted_only) {
+            if (!$dont_export_items) {
+                push @record_ids, $_->{biblionumber} for @{
+                    $dbh->selectall_arrayref(q| (
+                        SELECT biblio_metadata.biblionumber
+                        FROM biblio_metadata
+                          LEFT JOIN items USING(biblionumber)
+                        WHERE biblio_metadata.timestamp >= ?
+                          OR items.timestamp >= ?
+                    ) UNION (
+                        SELECT biblio_metadata.biblionumber
+                        FROM biblio_metadata
+                          LEFT JOIN deleteditems USING(biblionumber)
+                        WHERE biblio_metadata.timestamp >= ?
+                          OR deleteditems.timestamp >= ?
+                    ) |, { Slice => {} }, ( $timestamp ) x 4 );
+                };
+            } else {
+                push @record_ids, $_->{biblionumber} for @{
+                    $dbh->selectall_arrayref(q| (
+                        SELECT biblio_metadata.biblionumber
+                        FROM biblio_metadata
+                        WHERE biblio_metadata.timestamp >= ?
+                    ) |, { Slice => {} }, $timestamp );
+                };
+            }
+        }
+        if ($include_deleted || $deleted_only) {
+            push @deleted_record_ids, $_->{biblionumber} for @{
+                $dbh->selectall_arrayref(q|
+                    SELECT `biblionumber`
+                    FROM `deletedbiblio`
+                    WHERE `timestamp` >= ?
+                |, { Slice => {} }, $timestamp);
             };
         }
     } else {
@@ -252,6 +280,7 @@ else {
         {   record_type        => $record_type,
             record_ids         => \@record_ids,
             record_conditions  => @marc_conditions ? \@marc_conditions : undef,
+            deleted_record_ids => \@deleted_record_ids,
             format             => $output_format,
             csv_profile_id     => $csv_profile_id,
             export_items       => (not $dont_export_items),
@@ -268,7 +297,7 @@ export records - This script exports record (biblios or authorities)
 
 =head1 SYNOPSIS
 
-export_records.pl [-h|--help] [--format=format] [--date=datetime] [--record-type=TYPE] [--dont_export_items] [--deleted_barcodes] [--clean] [--id_list_file=PATH] --filename=outputfile
+export_records.pl [-h|--help] [--format=format] [--date=datetime] [--include_deleted] [--deleted_only] [--record-type=TYPE] [--dont_export_items] [--deleted_barcodes] [--clean] [--id_list_file=PATH] --filename=outputfile
 
 =head1 OPTIONS
 
@@ -288,6 +317,16 @@ Print a brief help message.
                         set (dd/mm/yyyy[ hh:mm:ss] for metric, yyyy-mm-dd[ hh:mm:ss] for iso,
                         mm/dd/yyyy[ hh:mm:ss] for us) records exported are the ones that
                         have been modified since DATETIME.
+
+=item B<--include_deleted>
+
+ --include_deleted      If enabled, when using --date option, deleted records will be included in export as marc records
+                        with leader record status set to "d" (deleted).
+
+=item B<--include_deleted>
+
+ --include_deleted      If enabled, when using --date option, only deleted records will be included in export as marc
+                        records with leader record status set to "d" (deleted).
 
 =item B<--record-type>
 