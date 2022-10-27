3e96427b5fe9525ce1cd9efb3c9a398e52fee097
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2022-10-27 09:22:56 +0200

    Add action to hook when checking in item that was not checked out

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -2571,6 +2571,15 @@ sub AddReturn {
         ) if C4::Context->preference('RealTimeHoldsQueue');
     }
 
+    if ( $doreturn and !$issue ) {
+        Koha::Plugins->call('after_circ_action', {
+            action  => 'checkin_no_issue',
+            payload => {
+                checkin => $item
+            }
+        });
+    }
+
     return ( $doreturn, $messages, $issue, ( $patron ? $patron->unblessed : {} ));
 }
 
