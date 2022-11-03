a206732157f10f9c39cfbdca47b6d82b90a1bfb2
David Gustafsson <david.gustafsson@ub.gu.se>
2022-11-03 14:26:34 +0100

    Bug 32092: Improve circulation rules cache utilization
    To test:
    1) Go to the page for placing a hold for a biblio with a large
       number of items
    2) Depending on the number of items this page can be very slow
       to load
    3) Ensure the following tests pass:
       t/db_dependent/Circulation.t
       t/db_dependent/Reserves.t
       t/db_dependent/Biblio.t
       t/db_dependent/Koha/Charges/Fees.t
       t/db_dependent/Items.t
       t/db_dependent/Koha/Patron.t
       t/db_dependent/api/v1/checkouts.t
       t/db_dependent/Koha/Template/Plugin/CirculationRules.t
       t/db_dependent/Template/Plugin/CirculationRules.t
       t/db_dependent/Koha/CirculationRules.t
       t/db_dependent/Circulation/Branch.t
       t/db_dependent/Circulation/TooMany.t
    3) Apply the patch
    4) The page loading time should now be significantly faster
    5) Ensure all the tests in 3) still pass
    
    Sponsored-by: Gothenburg University Library
    Signed-off-by: Emily Lamancusa <emily.lamancusa@montgomerycountymd.gov>
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- Koha/Patron.pm
+++ Koha/Patron.pm
@@ -1302,15 +1302,13 @@ sub can_request_article {
 
     $library_id //= C4::Context->userenv ? C4::Context->userenv->{'branch'} : undef;
 
-    my $rule = Koha::CirculationRules->get_effective_rule(
+    my $limit = Koha::CirculationRules->get_effective_rule_value(
         {
             branchcode   => $library_id,
             categorycode => $self->categorycode,
             rule_name    => 'open_article_requests_limit'
         }
     );
-
-    my $limit = ($rule) ? $rule->rule_value : undef;
 
     return 1 unless defined $limit;
 
@@ -1343,7 +1341,7 @@ sub article_request_fee {
 
     $library_id //= C4::Context->userenv ? C4::Context->userenv->{'branch'} : undef;
 
-    my $rule = Koha::CirculationRules->get_effective_rule(
+    my $value = Koha::CirculationRules->get_effective_rule_value(
         {
             branchcode   => $library_id,
             categorycode => $self->categorycode,
@@ -1351,9 +1349,7 @@ sub article_request_fee {
         }
     );
 
-    my $fee = ($rule) ? $rule->rule_value + 0 : 0;
-
-    return $fee;
+    return $value ? $value + 0 : 0;
 }
 
 =head3 add_article_request_fee_if_needed
