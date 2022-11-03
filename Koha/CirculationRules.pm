2fe313ff2d5618b037ece1dc81f78216e8e7cca8
David Gustafsson <david.gustafsson@ub.gu.se>
2022-11-03 14:26:34 +0100

    Bug 32092: Improve circulation rules cache utilization
    To test:
    1) Go to the page for placing a hold for a biblio with a large
       number of items
    2) Depending on the number of items this page can be very slow
       to load
    3) Ensure the following tests pass:
       t/db_dependent/Circulation.t
       t/db_dependent/Reserves.t
       t/db_dependent/Biblio.t
       t/db_dependent/Koha/Charges/Fees.t
       t/db_dependent/Koha/Charges/Fees.t
       t/db_dependent/Items.t
       t/db_dependent/Koha/Patron.t
       t/db_dependent/api/v1/checkouts.t
       t/db_dependent/Koha/Template/Plugin/CirculationRules.t
       t/db_dependent/Template/Plugin/CirculationRules.t
    3) Apply the patch
    4) The page loading time should now be significantly faster
    5) Ensure all the tests in 3) still pass
    
    Sponsored-by: Gothenburg University Library

--- Koha/CirculationRules.pm
+++ Koha/CirculationRules.pm
@@ -298,13 +298,16 @@ sub get_effective_rule_value {
     my $cache_key = sprintf "CircRules:%s:%s:%s:%s", $rule_name // q{},
       $categorycode // q{}, $branchcode // q{}, $itemtype // q{};
 
-    my $cached       = $memory_cache->get_from_cache($cache_key);
-    return $cached if $cached;
-
-    my $rule = $self->get_effective_rule($params);
-
-    my $value= $rule ? $rule->rule_value : undef;
-    $memory_cache->set_in_cache( $cache_key, $value );
+    my $value = $memory_cache->get_from_cache($cache_key);
+    unless (defined $value) {
+        my $rule = $self->get_effective_rule($params);
+        if ($rule) {
+            $value = $rule->rule_value;
+        }
+        $value //= 'undef';
+        $memory_cache->set_in_cache( $cache_key, $value );
+    }
+    return if $value eq 'undef';
     return $value;
 }
 
@@ -492,7 +495,7 @@ sub get_opacitemholds_policy {
 
     return unless $item or $patron;
 
-    my $rule = Koha::CirculationRules->get_effective_rule(
+    return Koha::CirculationRules->get_effective_rule_value(
         {
             categorycode => $patron->categorycode,
             itemtype     => $item->effective_itemtype,
@@ -500,8 +503,6 @@ sub get_opacitemholds_policy {
             rule_name    => 'opacitemholds',
         }
     );
-
-    return $rule ? $rule->rule_value : undef;
 }
 
 =head3 get_onshelfholds_policy
@@ -515,15 +516,14 @@ sub get_onshelfholds_policy {
     my $item = $params->{item};
     my $itemtype = $item->effective_itemtype;
     my $patron = $params->{patron};
-    my $rule = Koha::CirculationRules->get_effective_rule(
+    return Koha::CirculationRules->get_effective_rule_value(
         {
             categorycode => ( $patron ? $patron->categorycode : undef ),
             itemtype     => $itemtype,
             branchcode   => $item->holdingbranch,
             rule_name    => 'onshelfholds',
         }
-    );
-    return $rule ? $rule->rule_value : 0;
+    ) // 0;
 }
 
 =head3 get_lostreturn_policy
@@ -560,14 +560,12 @@ sub get_lostreturn_policy {
 
     my $branch = $behaviour_mapping->{ $behaviour };
 
-    my $rule = Koha::CirculationRules->get_effective_rule(
+    return Koha::CirculationRules->get_effective_rule_value(
         {
             branchcode => $branch,
             rule_name  => 'lostreturn',
         }
-    );
-
-    return $rule ? $rule->rule_value : 'refund';
+    ) // 'refund';
 }
 
 =head3 article_requestable_rules
@@ -648,7 +646,7 @@ sub get_effective_daysmode {
     my $itemtype         = $params->{itemtype};
     my $branchcode       = $params->{branchcode};
 
-    my $daysmode_rule = $class->get_effective_rule(
+    my $daysmode = $class->get_effective_rule_value(
         {
             categorycode => $categorycode,
             itemtype     => $itemtype,
@@ -656,12 +654,7 @@ sub get_effective_daysmode {
             rule_name    => 'daysmode',
         }
     );
-
-    return ( defined($daysmode_rule)
-          and $daysmode_rule->rule_value ne '' )
-      ? $daysmode_rule->rule_value
-      : C4::Context->preference('useDaysMode');
-
+    return $daysmode || C4::Context->preference('useDaysMode');
 }
 
 
