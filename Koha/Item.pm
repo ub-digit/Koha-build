d8002f5022e3067a38b09f236b6a1bd2e0078c8e
David Gustafsson <david.gustafsson@ub.gu.se>
2022-11-03 14:26:34 +0100

    Bug 32092: Improve circulation rules cache utilization
    To test:
    1) Go to the page for placing a hold for a biblio with a large
       number of items
    2) Depending on the number of items this page can be very slow
       to load
    3) Ensure the following tests pass:
       t/db_dependent/Circulation.t
       t/db_dependent/Reserves.t
       t/db_dependent/Biblio.t
       t/db_dependent/Koha/Charges/Fees.t
       t/db_dependent/Koha/Charges/Fees.t
       t/db_dependent/Items.t
       t/db_dependent/Koha/Patron.t
       t/db_dependent/api/v1/checkouts.t
       t/db_dependent/Koha/Template/Plugin/CirculationRules.t
       t/db_dependent/Template/Plugin/CirculationRules.t
       t/db_dependent/Koha/CirculationRules.t
    3) Apply the patch
    4) The page loading time should now be significantly faster
    5) Ensure all the tests in 3) still pass
    
    Sponsored-by: Gothenburg University Library
    Signed-off-by: Emily Lamancusa <emily.lamancusa@montgomerycountymd.gov>
    
    Signed-off-by: Kyle M Hall <kyle@bywatersolutions.com>

--- Koha/Item.pm
+++ Koha/Item.pm
@@ -1049,7 +1049,7 @@ sub article_request_type {
       :                                      undef;
     my $borrowertype = $borrower->categorycode;
     my $itemtype = $self->effective_itemtype();
-    my $rule = Koha::CirculationRules->get_effective_rule(
+    return Koha::CirculationRules->get_effective_rule_value(
         {
             rule_name    => 'article_requests',
             categorycode => $borrowertype,
@@ -1057,9 +1057,6 @@ sub article_request_type {
             branchcode   => $branchcode
         }
     );
-
-    return q{} unless $rule;
-    return $rule->rule_value || q{}
 }
 
 =head3 current_holds
