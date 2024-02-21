magnuskull/Koha

Bug 18138: Use MARC modification template when adding/updating bibliographic records
Add a preference that enables a specific marc modification template to be applied on bibliographic records saved using simple or advanced MARC editor or the HTTP API.

To test:
1) Create a MARC modification template containg one or more actions.
2) Under "Global system preferences" and "Cataloging" set the "EditBiblioMarcModificationTemplate" preference to the name of the previously created template.
3) Edit (or create) a bibliographic record using either the simple or advanced MARC editor.
4) Save the record.
5) Verify that the action(s) have been applied.

Sponsored-by: Gothenburg University Library

cataloguing/addbiblio.pl
@@ -34,6 +34,7 @@ use C4::Koha;
 use C4::ClassSource;
 use C4::ImportBatch;
 use C4::Charset;
+use C4::MarcModificationTemplates;
 use Koha::BiblioFrameworks;
 use Koha::DateUtils;
 
@@ -46,6 +47,7 @@ use Date::Calc qw(Today);
 use MARC::File::USMARC;
 use MARC::File::XML;
 use URI::Escape;
+use List::MoreUtils qw(firstval);
 
 if ( C4::Context->preference('marcflavour') eq 'UNIMARC' ) {
     MARC::File::XML->default_record_format('UNIMARC');
@@ -836,7 +838,17 @@ if ( $op eq "addbiblio" ) {
         my $oldbibitemnum;
         if (C4::Context->preference("BiblioAddsAuthorities")){
             BiblioAutoLink( $record, $frameworkcode );
-        } 
+        }
+        my $marc_modification_template_name = C4::Context->preference("EditBiblioMarcModificationTemplate");
+        if ($marc_modification_template_name) {
+            my $template = firstval { $_->{'name'} eq $marc_modification_template_name } GetModificationTemplates();
+            if ($template) {
+                ModifyRecordWithTemplate($template->{'template_id'}, $record);
+            }
+            else {
+                warn "No MARC modification template exists with name \"$marc_modification_template_name\"";
+            }
+        }
         if ( $is_a_modif ) {
             ModBiblio( $record, $biblionumber, $frameworkcode );
         }

ub-digit/Koha-build

cataloguing/addbiblio.pl
@@ -44,6 +44,7 @@ use MARC::Record;
 use C4::ClassSource qw( GetClassSources );
 use C4::ImportBatch qw( GetImportRecordMarc );
 use C4::Charset qw( SetMarcUnicodeFlag );
+use C4::MarcModificationTemplates;
 use Koha::BiblioFrameworks;
 use Koha::DateUtils qw( dt_from_string );
 
@@ -58,6 +59,7 @@ use Koha::UI::Form::Builder::Biblio;
 use MARC::File::USMARC;
 use MARC::File::XML;
 use URI::Escape qw( uri_escape_utf8 );
+use List::MoreUtils qw(firstval);
 
 if ( C4::Context->preference('marcflavour') eq 'UNIMARC' ) {
     MARC::File::XML->default_record_format('UNIMARC');
@@ -674,6 +676,16 @@ if ( $op eq "addbiblio" ) {
     # it is not a duplicate (determined either by Koha itself or by user checking it's not a duplicate)
     if ( !$duplicatebiblionumber or $confirm_not_duplicate ) {
         my $oldbibitemnum;
+        my $marc_modification_template_name = C4::Context->preference("SaveBiblioMarcModificationTemplate");
+        if ($marc_modification_template_name) {
+            my $template = firstval { $_->{'name'} eq $marc_modification_template_name } GetModificationTemplates();
+            if ($template) {
+                ModifyRecordWithTemplate($template->{'template_id'}, $record);
+            }
+            else {
+                warn "No MARC modification template exists with name \"$marc_modification_template_name\"";
+            }
+        }
         if ( $is_a_modif ) {
             my $member = Koha::Patrons->find($loggedinuser);
             ModBiblio(