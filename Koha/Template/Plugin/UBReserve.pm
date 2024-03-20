be1785368fe61b9bb99684dfb61a10d20ed4e574
Per Larsson <per.larsson@ub.gu.se>
2024-03-20 15:52:11 +0100

    Extend isPickup to handle patron category

--- Koha/Template/Plugin/UBReserve.pm
+++ Koha/Template/Plugin/UBReserve.pm
@@ -1,12 +1,16 @@
 package Koha::Template::Plugin::UBReserve;
 use Modern::Perl;
 use base qw( Template::Plugin );
+use Koha::Patrons;
 
 sub isPickup {
     my ($self, $reserve) = @_;
     return 1 if (!defined $reserve);
     my $reservenotes = $reserve->reservenotes();
-    return 0 if ($reservenotes =~ /^L.netyp: Skicka hem/);
+    return 0 if ($reservenotes =~ /^L.netyp: Skicka/);
+    my $borrowernumber = $reserve->borrowernumber();
+    my $categorycode = Koha::Patrons->find($borrowernumber)->categorycode();
+    return 0 if ($categorycode =~ /^B/);
     return 1;
 }
 
