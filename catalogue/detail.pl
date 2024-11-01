ub-digit/Koha

Show 095a when item has no callnumber

catalogue/detail.pl
@@ -367,6 +367,7 @@ $template->param(
 	analytics_flag	=> $analytics_flag,
 	C4::Search::enabled_staff_search_views,
         materials       => $materials_flag,
+        bibcallnumber => $record->subfield('095', 'a')
 );
 
 if (C4::Context->preference("AlternateHoldingsField") && scalar @items == 0) {

ub-digit/Koha-build

catalogue/detail.pl
@@ -490,6 +490,7 @@ $template->param(
     analytics_flag          => $analytics_flag,
     C4::Search::enabled_staff_search_views,
     materials => $materials_flag,
+    bibcallnumber => $marc_record->subfield('095', 'a')
 );
 
 if (C4::Context->preference("AlternateHoldingsField") && $items->count == 0) {