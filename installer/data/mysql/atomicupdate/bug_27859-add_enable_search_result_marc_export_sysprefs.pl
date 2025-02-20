d74511d7ffe591e881ac3f3f48609dd50de5a69c
David Gustafsson <david.gustafsson@ub.gu.se>
2025-02-20 12:43:20 +0100

    Bug 27859: (follow-up) Don't enable feature by default

--- installer/data/mysql/atomicupdate/bug_27859-add_enable_search_result_marc_export_sysprefs.pl
+++ installer/data/mysql/atomicupdate/bug_27859-add_enable_search_result_marc_export_sysprefs.pl
@@ -7,7 +7,7 @@ return {
         my ($args) = @_;
         my ($dbh, $out) = @$args{qw(dbh out)};
 
-        $dbh->do(q{ INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('EnableElasticsearchSearchResultExport', 1, NULL, 'Enable search result export', 'YesNo') });
+        $dbh->do(q{ INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('EnableElasticsearchSearchResultExport', 0, NULL, 'Enable search result export', 'YesNo') });
         say $out "Added new system preference 'EnableElasticsearchSearchResultExport'";
 
         $dbh->do(q{ INSERT IGNORE INTO systempreferences (variable, value, options, explanation, type) VALUES ('ElasticsearchSearchResultExportCustomFormats', '', NULL, 'Search result export custom formats', 'textarea') });
