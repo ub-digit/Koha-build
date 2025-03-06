ff302cbee33ad8f1afd40b4ea8f00bf5aa875a79
Stefan Berndtsson <stefan.berndtsson@ub.gu.se>
2025-03-06 13:09:45 +0100

    Always write firstname into both firstname and preferred_name so that there is no mixup between them

--- Koha/Patron.pm
+++ Koha/Patron.pm
@@ -229,9 +229,12 @@ sub store {
                 if C4::Context->preference("uppercasesurnames");
 
             # Add preferred name unless specified
-            unless ( $self->preferred_name ) {
-                $self->preferred_name( $self->firstname );
-            }
+
+            # GUB: Disable check for preferred name and always set it to firstname
+            # unless ( $self->preferred_name ) {
+            #     $self->preferred_name( $self->firstname );
+            # }
+            $self->preferred_name( $self->firstname );
 
             $self->relationship(undef)    # We do not want to store an empty string in this field
                 if defined $self->relationship
